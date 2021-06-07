94d3fc9f5b8970c19e5b3768dd49ad0730a08b94
QuantumCode2000 <tancaraninajorgeluis@gmail.com>
2021-06-07 18:05:35 -0400

    terminando el copyright del footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,25 @@
-clear
+import React from 'react';
+import "./styles/Footer.css"
+import logoCopyright from "../images/Frame.svg"
+const Footer = () => {
+    return (
+        <React.Fragment>
+            <footer>
+                <div className="footer-top">
+                    <div className="about">ab</div>
+                    <div className="archives">ar</div>
+                    <div className="newsletter">news</div>
+                    <div className="recipes">rec</div>
+                </div>
+                <div className="footer-bottom">
+                    <div className="copyright">
+                        <img src={logoCopyright} alt="" />
+                        <p>Copyright 2020 by <strong>ojjomedia</strong>. All Rights Reserved</p>
+                    </div>
+                </div>
+            </footer>
+        </React.Fragment>
+    );
+};
+
+export default Footer;
