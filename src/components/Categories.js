0ad4051622b61438767fcef2e91e247ccb8557d9
VictorviruzZ2 <victorvf13.vvf@gmail.com>
2021-06-08 19:56:01 -0400

    Categories y SubscribeFollow acabado

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -4,10 +4,23 @@ export default function Categories() {
     return (
         <>
             <div className = "conteiner categories">
-                <div className = "card">
-                    <img src = "./images/image1.jpg" alt=""/>
-                </div>
+                <div className = "card"></div>
             </div>
+            <p className="mg">Mexican Grilled Corn Recipe</p>
+            <p className="rc">Rost Chicken With Lemon Gravy</p>
+            <p className="am">Avocado Melon sald With Lime Vinagrette</p>
+            <p className="cb">Chunky Beef Stew</p>
+            <p className="fv">Farmhouse Vegetable And Barley Soup</p>
+            <div className = "img1">
+            <img src = "" alt=""/></div>
+            <div className = "img2">
+            <img src = "" alt=""/></div>
+            <div className = "img3">
+            <img src = "" alt=""/></div>
+            <div className = "img4">
+            <img src = "" alt=""/></div>
+            <div className = "img5">
+            <img src = "" alt=""/></div>
         </>
     )
 }
