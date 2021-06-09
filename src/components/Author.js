a8f99b0300ec8c40d9b1f233ad482930fae38db7
emanuel <emanuel79581210@gmail.com>
2021-06-08 22:35:13 -0400

    arreglando estilos

--- src/components/Author.js
+++ src/components/Author.js
@@ -1,15 +1,14 @@
 import React from 'react'
+import './styles/Author.css'
 
 export default function Author() {
     return (
-        <>
-             <div class='Author_cont'>
-        <h2 class="Author">Author</h2>
-        <svg width="270" height="1" viewBox="0 0 270 1" fill="none" xmlns="http://www.w3.org/2000/svg"><line x1="4.37114e-08" y1="0.5" x2="270" y2="0.500024" stroke="#2D2D2D"/></svg>
-        <img class="iau" src=" https://i13.servimg.com/u/f13/20/31/62/11/image10.png" alt="">
-        <h3 class="Luice">Luice Fonci</h3>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Justo cursus velic comodo</p>
-        </div>
-        </>
+            <div class='Author_cont'>
+                <h2 class="Author">Author</h2>
+                <svg width="270" height="1" viewBox="0 0 270 1" fill="none" xmlns="http://www.w3.org/2000/svg"><line x1="4.37114e-08" y1="0.5" x2="270" y2="0.500024" stroke="#2D2D2D"/></svg>
+                <img class="iau" src=" https://i13.servimg.com/u/f13/20/31/62/11/image10.png" alt=""/>
+                <h3 class="Luice">Luice Fonci</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Justo cursus velic comodo</p>
+            </div>
     )
 }
