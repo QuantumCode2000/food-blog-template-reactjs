20fd9a4fc9861b74ee95cad2f0ee5e1480c99292
VictorviruzZ2 <victorvf13.vvf@gmail.com>
2021-06-08 22:47:57 -0400

    Realizando cambios desde la rama Sub

--- src/index.js
+++ src/index.js
@@ -10,7 +10,4 @@ const element2 = <SubscribeFollow/>;
 ReactDOM.render(element, conteiner);
 ReactDOM.render(element2, conteiner2);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
